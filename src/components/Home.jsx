f12da45ba63f3446cbaa967b3bbccbd25c36104e
Yakir Ben Harosh <yakir1992@gmail.com>
2022-06-26 01:52:15 +0300

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import {HiArrowNarrowRight} from 'react-icons/hi'
-import {Link} from 'react-scroll'
+
 
 
 const Home = () => {
@@ -15,8 +15,8 @@ const Home = () => {
         <p className="text-[#8892b0] py-4 max-w-[700px]">I am a Full Stack Web Developer with experience of more than 6 years with specializing in Web-Development & Design 
         </p>
         <div>
-            <button className="text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600"><Link to="work" spy={true} smooth={true} duration={500}>
-        View Work</Link> 
+            <button className="text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600">
+        View Work
             <span className='group-hover:rotate-90 duration-300'>
             <HiArrowNarrowRight className="ml-3"/>
             </span>
