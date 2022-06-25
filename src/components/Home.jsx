689bbed1615f4d13a0d41207e3520106977d66d4
yakir1992 <yakir1992@gmail.com>
2022-06-26 01:44:49 +0300

    upload

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -12,8 +12,7 @@ const Home = () => {
         <p className="text-pink-600 text-5xl">Hi, My name is</p>
         <h1 className="text-4xl sm:text-7xl font-bold text-[#ccd6f6]">Yakir Ben Harosh</h1>
         <h2 className="text-4xl sm:text-7xl font-bold text-[#8892b0]">I'm A Full Stack Developer</h2>
-        <p className="text-[#8892b0] py-4 max-w-[700px]">I'm A Full
-        I am Anas Tuebingmah.I am a Full Stack Web Developer with experience of more than 6 years with specializing in Web-Development & Design 
+        <p className="text-[#8892b0] py-4 max-w-[700px]">I am a Full Stack Web Developer with experience of more than 6 years with specializing in Web-Development & Design 
         </p>
         <div>
             <button className="text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600"><Link to="work" spy={true} smooth={true} duration={500}>
