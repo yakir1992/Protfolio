5f39c58dce1100332ac79b966cc3f86d65eca56a
yakir1992 <yakir1992@gmail.com>
2022-06-26 01:54:48 +0300

    changed done

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
   return (
     <div name='home' className='w-full h-screen bg-[#0a192f]'>
 
-    {/* Container */}
+    {/* Container1 */}
     div <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full ">
         <p className="text-pink-600 text-5xl">Hi, My name is</p>
         <h1 className="text-4xl sm:text-7xl font-bold text-[#ccd6f6]">Yakir Ben Harosh</h1>
