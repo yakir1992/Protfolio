9e4b4f8d547a9d2dd3db72f84518a2b5101b4dee
yakir1992 <yakir1992@gmail.com>
2022-06-26 21:08:36 +0300

    changed

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import workImg from '../assets/workImg.jpeg'
 import realestate from '../assets/realestate.jpg'
+import sunset from '../assets/sunset.jpg'
 
 const Work = () => {
   return (
@@ -37,17 +38,17 @@ const Work = () => {
                 
                 
 {/* Grid Item */}
-<div style={{backgroundImage: `url(${realestate})`}} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+<div style={{backgroundImage: `url(${sunset})`}} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
                     {/* Hover effects */}
                     <div className='opacity-0 group-hover:opacity-100'>    
                     <span className='text-2xl font-bold text-white tracking-wider'>
-                    React JS Application
+                    React JS Weather App 
                     </span>
                          <div className='pt-8 text-center'>
-                        <a href="/">
+                        <a href="https://weather-app-react1.netlify.app/">
                             <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
                         </a>
-                        <a href="/">
+                        <a href="https://github.com/yakir1992/weather-app-react">
                             <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
                         </a>
                    
