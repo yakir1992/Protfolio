9c7045b9bdb8992b23cf5ef508efd4dd01e22dd0
yakir1992 <yakir1992@gmail.com>
2022-06-26 03:12:57 +0300

    changed

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,9 +10,9 @@ const Home = () => {
     {/* Container1 */}
     div <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full ">
         <p className="text-pink-600 text-5xl">Hi, My name is</p>
-        <h1 className="text-4xl sm:text-7xl font-bold text-[#ccd6f6]">Yakir Ben Harosh</h1>
+        <h1 className="text-4xl sm:text-7xl font-bold text-[#ccd6f6]">Yakir</h1>
         <h2 className="text-4xl sm:text-7xl font-bold text-[#8892b0]">I'm A Full Stack Developer</h2>
-        <p className="text-[#8892b0] py-4 max-w-[700px]">I am a Full Stack Web Developer with experience of more than 6 years with specializing in Web-Development & Design 
+        <p className="text-[#8892b0] py-4 max-w-[700px]">I am a self-taught web developer from Israel. After discovering my passion for web development, I couldn’t get enough. I regularly create websites and web applications for my own portfolio, keep sharpening the saw, always thirsty for more knowledge and looking to be up to date with technology. I am looking forward to bringing that passion to a full-time role.
         </p>
         <div>
             <button className="text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600">
