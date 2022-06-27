ecb925bb156fd20bbd8de00d12d80ca07bae252f
Yakir Ben Harosh <yakir1992@gmail.com>
2022-06-27 13:36:58 +0300

    changes on components

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
-import {HiArrowNarrowRight} from 'react-icons/hi'
+import {HiArrowNarrowRight, HiFingerPrint} from 'react-icons/hi'
 import {Link} from 'react-scroll'
+
+
 
 
 const Home = () => {
