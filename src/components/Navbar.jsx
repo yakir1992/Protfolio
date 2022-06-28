6c6ff9c200aa98d64bb5b44c618d05a5c17626cb
YakyAK <yakir1992@gmail.com>
2022-06-28 13:14:35 +0300

    changed social links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -76,7 +76,7 @@ const Navbar = () => {
   </li>
   <li className='w-[160px] h-[60px] flex justify-between items-center items-center ml-[-110px] hover:ml-[-10px] duration-300 bg-[#333333]'>
     <a className='flex justify-between  w-full text-gray-300'
-    href="https://github.com/yakir1992">
+    href="https://github.com/yakir1992"target="_blank" rel="noreferrer">
       GitHub <FaGithub size={30}/>
     </a>
   </li>
