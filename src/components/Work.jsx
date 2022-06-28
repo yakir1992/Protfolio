f2deccaae519313d85118c9dde2e7824ceec4950
YakyAK <yakir1992@gmail.com>
2022-06-28 12:21:16 +0300

    commit changes

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -22,7 +22,7 @@ const Work = () => {
                         {/* Hover effects */}
                         <div className='opacity-0 group-hover:opacity-100'>
                             <span className='text-2xl font-bold text-white tracking-wider'>
-                                Face Detection 
+                                Face Detection.
                             </span>
                             <div className='pt-8 text-center'>
                                 <a href="https://face-detectionz.netlify.app/">
